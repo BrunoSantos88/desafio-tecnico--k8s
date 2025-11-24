bfa3c504decc4086f04fbd951dbb76d6ee967b42
brsn <brsn@brsn.brsn>
2025-11-24 02:55:08 -0300

    desafisok8s

--- app-express-otel/backend/server.js
+++ app-express-otel/backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors');
+
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-
-const cors = require('cors');
 app.use(cors({
   origin: '*'
 }));
